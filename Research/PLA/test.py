b06b486b0f4e6a7cd851aa2b4f414acf58daaf3d
ETS-Hashemi <pmhashemi@gmail.com>
2025-04-18 23:36:59 -0400

    fix: correct comment to include complete description of the test script

--- Research/PLA/test.py
+++ Research/PLA/test.py
@@ -1,3 +1,3 @@
-# Research/PLA/your_script.py it is a test to test the performance 
+# Research/PLA/your_script.py it is a test to test the performance of the code. 
 print("This is a test script to test the performance of the code.")
 
