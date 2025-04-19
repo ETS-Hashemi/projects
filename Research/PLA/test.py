76319e93ad94db65afb7ac54925fcbd1941d7e73
ETS-Hashemi <pmhashemi@gmail.com>
2025-04-18 23:20:53 -0400

    fix: remove redundant comment from test script

--- Research/PLA/test.py
+++ Research/PLA/test.py
@@ -1,3 +1,2 @@
 # Research/PLA/your_script.py it is a test to test  the performance of the code 
 print("This is a test script to test the performance of the code.")
-# It will not run the actual code but will just print a message.
