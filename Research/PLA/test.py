5b3b233b4dd6dc872c820068ed9ca4ec2c5caecc
ETS-Hashemi <pmhashemi@gmail.com>
2025-04-18 23:17:24 -0400

    fix: add comment to clarify the purpose of the test script

--- Research/PLA/test.py
+++ Research/PLA/test.py
@@ -1,2 +1,3 @@
 # Research/PLA/your_script.py it is a test to test  the performance of the code 
 print("This is a test script to test the performance of the code.")
+# It will not run the actual code but will just print a message.
